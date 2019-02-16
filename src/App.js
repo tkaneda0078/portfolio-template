131ef63dfa27efda384e225cb8aa0aa427f26d75
tkaneda0078 <1111510078r21@gmail.com>
2019-02-16 16:15:08 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,56 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+    return(
+      <div className='App'>
+
+        {/* header */}
+        <header className='header'>
+          <h1 className='header-title'>Portfolio</h1>
+          <nav className='headerNav'>
+            <a className='headerNav-item' href='#about'>About</a>
+            <a className='headerNav-item' href='#works'>Works</a>
+          </nav>
         </header>
+
+        {/* profile */}
+        <div className='me'>
+          <h1 className='me-name'>payapaya</h1>
+          <p className='me-description'>Web Engineer</p>
+        </div>
+
+        {/* about */}
+        <section className='section' id='about'>
+          <h2 className='section-title'>About</h2>
+          <p className='about-description'>
+            Hello, I'm WEB Engineer.
+          </p>
+        </section>
+
+        {/* works */}
+        <section className='section' id='works'>
+          <h2 className='section-title'></h2>
+          <ul>
+            <li className='work-item'>
+              <img className='work-item-img' src='https://dummyimage.com/600x300/eee/ccc' />
+            </li>
+            <h3 className='work-item-category'>2018 - react</h3>
+            <h3 className='work-item-title'>sample1</h3>
+          </ul>
+          <ul>
+            <li className='work-item'>
+              <img className='work-item-img' src='https://dummyimage.com/600x300/eee/ccc' />
+            </li>
+            <h3 className='work-item-category'>2019 - react</h3>
+            <h3 className='work-item-title'>sample2</h3>
+          </ul>
+        </section>
+
+        {/* footer */}
+        <footer>
+          Copyright &copy; samle. All right All Rights reserved.
+        </footer>
+
       </div>
     );
   }
